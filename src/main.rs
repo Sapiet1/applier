90a5c8c09d4b89d8399a0393324d97a2493907fe
Sapiet1 <3cvang24@gmail.com>
2024-01-05 10:56:45 -0600

    format changes

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,130 @@
 use std::{
-    path::PathBuf,
+    env,
     ffi::OsString,
     io::ErrorKind,
+    path::PathBuf,
     process::{self, Stdio},
-    env,
 };
 
 use clap::{Parser, Subcommand};
 
 use tokio::{
     fs::{self, DirEntry},
-    process::Command,
     io::{self, AsyncWriteExt},
+    process::Command,
     sync::Mutex,
 };
 
 use tokio_stream::wrappers::ReadDirStream;
 
-use futures::{
-    StreamExt,
-    stream,
-    future,
-};
+use futures::{future, stream, StreamExt};
 
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
 
     let External::Command(command) = cli.command;
-    let [command, args @ ..] = command.as_slice() else { unreachable!() };
+    let [command, args @ ..] = command.as_slice() else {
+        unreachable!()
+    };
 
-    let directory = cli.path.unwrap_or_else(|| env::current_dir().unwrap_or_else(|error| {
-        eprintln!("Failed while reading the parent directory: {}", error);
-        process::exit(1);
-    }));
+    let parent_error = |error: io::Error| -> ! {
+        handler::exit!("Failed while reading the parent directory: {}", error)
+    };
 
-    let entries = fs::read_dir(&directory).await.unwrap_or_else(|error| {
-        eprintln!("Failed while reading the parent directory: {}", error);
-        process::exit(1);
-    });
+    let directory = cli
+        .path
+        .unwrap_or_else(|| env::current_dir().unwrap_or_else(|error| parent_error(error)));
 
-    let ignored_directories = &*future::try_join_all(cli.ignore
-        .into_iter()
-        .map(|path| {
-            if path.is_relative() {
-                let mut ignored_directory = directory.clone();
-                ignored_directory.push(path);
-                fs::canonicalize(ignored_directory)
-            } else {
-                fs::canonicalize(path)
-            }
-        }))
+    let entries = fs::read_dir(&directory)
         .await
-        .unwrap_or_else(|error| {
-            eprintln!("Failed while reading ignored directories: {}", error);
-            process::exit(1);
-        });
+        .unwrap_or_else(|error| parent_error(error));
+
+    let ignored_directories = &*future::try_join_all(cli.ignore.into_iter().map(|path| {
+        if path.is_relative() {
+            let mut ignored_directory = directory.clone();
+            ignored_directory.push(path);
+            fs::canonicalize(ignored_directory)
+        } else {
+            fs::canonicalize(path)
+        }
+    }))
+    .await
+    .unwrap_or_else(|error| handler::exit!("Failed while reading ignored directories: {}", error));
 
     let stderr = &Mutex::new(io::stderr());
     let stdout = &Mutex::new(io::stdout());
-    ReadDirStream::new(entries).for_each_concurrent(None, |entry| async move {
-        let result = async move {
-            let entry = entry
-                .as_ref()
-                .map(DirEntry::path)
-                .map_err(|error| format!("Error occurred: {}", error))?;
+    ReadDirStream::new(entries)
+        .for_each_concurrent(None, |entry| async move {
+            let result = async move {
+                let entry = entry
+                    .as_ref()
+                    .map(DirEntry::path)
+                    .map_err(|error| format!("Error occurred: {}", error))?;
 
-            if !entry.is_dir()
-            || stream::iter(ignored_directories).any(|path| {
-                let entry = &entry;
-                async move { fs::canonicalize(entry).await.is_ok_and(|entry| &entry == path) }
-            })
-            .await
-            {
-                return Ok(());
+                let ignore = !entry.is_dir()
+                    || stream::iter(ignored_directories)
+                        .any(|path| {
+                            let entry = &entry;
+                            async move {
+                                fs::canonicalize(entry)
+                                    .await
+                                    .is_ok_and(|entry| &entry == path)
+                            }
+                        })
+                        .await;
+
+                if ignore {
+                    return Ok(());
+                }
+
+                let output = Command::new(command)
+                    .current_dir(&entry)
+                    .stdout(Stdio::null())
+                    .stderr(Stdio::piped())
+                    .args(args)
+                    .spawn()
+                    .map_err(|error| {
+                        if error.kind() == ErrorKind::NotFound {
+                            handler::exit!("Command {:?} could not be found", command)
+                        } else {
+                            format!("Error occurred at {:?}: {}", entry, error)
+                        }
+                    })?
+                    .wait_with_output()
+                    .await
+                    .map_err(|error| format!("Error occurred at {:?}: {}", entry, error))?;
+
+                let mut stdout = stdout.lock().await;
+                stdout
+                    .write_all(
+                        format!(
+                            "Command {:?} at {:?} returned {}\n",
+                            command, entry, output.status
+                        )
+                        .as_bytes(),
+                    )
+                    .await
+                    .expect("failed writing to stdout");
+                stdout
+                    .write_all(&output.stderr)
+                    .await
+                    .expect("failed writing to stdout");
+
+                Ok::<(), String>(())
             }
+            .await;
 
-            let output = Command::new(command)
-                .current_dir(&entry)
-                .stdout(Stdio::null())
-                .stderr(Stdio::piped())
-                .args(args)
-                .spawn()
-                .map_err(|error| {
-                    if error.kind() == ErrorKind::NotFound {
-                        eprintln!("Command {:?} could not be found", command);
-                        process::exit(1);
-                    } else {
-                        format!("Error occurred at {:?}: {}", entry, error)
-                    }
-                })?
-                .wait_with_output()
-                .await
-                .map_err(|error| format!("Error occurred at {:?}: {}", entry, error))?;
-
-            let mut stdout = stdout.lock().await;
-            stdout
-                .write_all(format!("Command {:?} at {:?} returned {}\n", command, entry, output.status).as_bytes())
-                .await
-                .expect("failed writing to stdout");
-            stdout
-                .write_all(&output.stderr)
-                .await
-                .expect("failed writing to stdout");
-
-            Ok::<(), String>(())
-        }
+            if let Err(error_message) = result {
+                stderr
+                    .lock()
+                    .await
+                    .write_all(error_message.as_bytes())
+                    .await
+                    .expect("failed writing to stderr");
+            }
+        })
         .await;
-
-        if let Err(error_message) = result {
-            stderr
-                .lock()
-                .await
-                .write_all(error_message.as_bytes())
-                .await
-                .expect("failed writing to stderr");
-        }
-    })
-    .await;
 }
 
 #[derive(Parser)]
@@ -138,4 +145,17 @@ struct Cli {
 enum External {
     #[command(external_subcommand)]
     Command(Vec<OsString>),
+}
+
+mod handler {
+    macro_rules! exit {
+        ($( $tokens:tt )*) => {
+            {
+                eprintln!($( $tokens )*);
+                process::exit(1);
+            }
+        };
+    }
+
+    pub(crate) use exit;
 }
